cd5ea1d97672d074c209f2bd1cfb08400e86e70a
Bojana Stajić <117362591+little-software-engineer@users.noreply.github.com>
2025-09-20 16:25:50 +0200

    Add files via upload
    Note: Create your own .env.local file that will let you store your sensitive data.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        spotifyBlack: "#191414",
+        spotifyGreen: "#1DB954",
+        spotifyGray: "#282828",
+      },
+    },
+  },
+  plugins: [],
+}
